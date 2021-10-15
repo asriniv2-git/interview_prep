716882fb87d6f18ffa2761a9bb1c64c5a4fa451b
asriniv2-git <64177292+asriniv2-git@users.noreply.github.com>
2021-10-14 22:02:25 -0700

    Update merge_sorted_arrays.cpp

--- merge_sorted_arrays.cpp
+++ merge_sorted_arrays.cpp
@@ -19,7 +19,6 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = 0;
         int j = 0;
-        int k = 0;
         vector<int> nums3;
         
         while (i < m && j < n) {
@@ -33,15 +32,13 @@ public:
             }
         }
         
-        if (i < m) {
-            for (k = i; k < m; k++) {
-                nums3.push_back(nums1[k]);
-            }
+        while (i < m) {
+            nums3.push_back(nums1[i]);
+            i++;
         }
-        else if (j < n) {
-            for (k = j; k < n; k++) {
-                nums3.push_back(nums2[k]);
-            }
+        while (j < n) {
+            nums3.push_back(nums2[j]);
+            j++;
         }
         
         for (i = 0; i < m+n; i++) {
